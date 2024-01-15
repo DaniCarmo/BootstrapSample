99aa703455a16e36d3e0bca91adc9d41bc4ec52e
DaniCarmo <dani93elle@hotmail.com>
2024-01-15 13:11:29 +0000

    add event listener to shop card btns

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,7 @@
-// JavaScript code for associated HTML document
+/* JavaScript code for associated HTML document
 var salary = 0.00;
 var products = 0;
+*/
 
 /* Page Loading event listener
 document.addEventListener("DOMContentLoaded", function () {
@@ -9,24 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // Bootstrap card clicked event listener
-document.getElementById("card003").addEventListener("click", function()
-{     
-    var element = document.getElementById("card003");
-    if (element.innerHTML=="Add to cart")
-    {
-	    element.innerHTML = "Remove from cart";
-        products = 1;        
-        element.style.background = "pink";        
-    }
-    else
-    {
-	    element.innerHTML = "Add to cart";
-        products = 0;
-        element.style.background = "yellow";
-    }
-}); 
+var elements = document.getElementsByClassName("card003");
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function () {
+        if (this.innerHTML == "Add to cart") {
+            this.innerHTML = "Remove from cart";
+            products = 1;
+            this.style.background = "pink";
+        } else {
+            this.innerHTML = "Add to cart";
+            products = 0;
+            this.style.background = "yellow";
+        }
+    });
+}
 
 // Bootstrap card clicked event listener
 document.getElementById("btnCart").addEventListener("click", function () {
-    alert("Thank you for your purchase of:\nFord GTI\nPrice: 129.00");
+    alert("Thank you for your purchase!");
 }); 
